87663955f4327c9ab726ad628ed6de6b7178a6f1
GowthamaViknesh <gowthampostbox18@gmail.com>
2023-10-21 15:46:58 +0530

    ChatApp

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5000';
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/chats' element={<Chatpage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
